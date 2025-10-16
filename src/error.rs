dbe2d388985f10306fbf8617f05c02d56950d16a
Raymond Nambaale <nambaaler@gmail.com>
2025-10-16 13:58:50 +0300

    Implements password util

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use axum::{
     response::{IntoResponse, Response},
     Json,
 };
+
 use serde_json::json;
 use thiserror::Error;
 use tracing::{event, Level};
@@ -12,32 +13,20 @@ pub enum BunnyChessApiError {
     #[error("DB Error {0}")]
     Db(#[from] sqlx::Error),
 
-    #[error("Invalid amount: {0}")]
-    InvalidAmount(String),
-
-    #[error("Invalid quote {0}")]
-    InvalidQuote(String),
-
     #[error("{0}")]
-    SwapAmountMismatch(String),
-
-    #[error("duplicate promises.")]
-    SwapHasDuplicatePromises,
+    HashError(String),
 
     #[error("Invalid quote uuid {0}")]
     InvalidUuid(#[from] uuid::Error),
 
-    #[error("Not Enough tokens. Required amount {0}")]
-    NotEnoughTokens(u64),
-
-    #[error("Proof already used {0}")]
-    ProofAlreadyUsed(String),
-
-    #[error("PrivateKey in keyset not found")]
-    PrivateKeyNotFound,
-
     #[error("User already exists")]
     UserAlreadyExists,
+
+    #[error("Task execution error: {0}")]
+    SpawnTaskError(#[from] tokio::task::JoinError),
+
+    #[error("{0}")]
+    InvalidInputError(String),
 }
 
 impl IntoResponse for BunnyChessApiError {
@@ -51,4 +40,10 @@ impl IntoResponse for BunnyChessApiError {
 
         (StatusCode::BAD_REQUEST, body).into_response()
     }
+}
+
+impl From<argon2::password_hash::Error> for BunnyChessApiError {
+  fn from(value: argon2::password_hash::Error) -> Self {
+    BunnyChessApiError::HashError(value.to_string())
+  }
 }
