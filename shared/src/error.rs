623b278eb92f24dbf59979552f2792b3ccd36b09
Raymond Nambaale <nambaaler@gmail.com>
2025-10-29 08:39:18 +0300

    Convert API core error to tonic status

--- shared/src/error.rs
+++ shared/src/error.rs
@@ -7,4 +7,13 @@ pub enum BunnyChessApiError {
 
     #[error(transparent)]
     RedisError(#[from] redis::RedisError),
+
+    #[error("{0}")]
+    UnknownGameTypeError(String),
+}
+
+impl From<BunnyChessApiError> for tonic::Status {
+    fn from(error: BunnyChessApiError) -> Self {
+        tonic::Status::invalid_argument(error.to_string())
+    }
 }
