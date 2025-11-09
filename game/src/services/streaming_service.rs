9d9b5b68fe800e8d8c5d2632d825c9e98c7d8c6b
Raymond Nambaale <nambaaler@gmail.com>
2025-11-09 08:49:58 +0300

    [game] Implements more game events

--- game/src/services/streaming_service.rs
+++ game/src/services/streaming_service.rs
@@ -1,4 +1,4 @@
-use shared::{error::BunnyChessApiError, events::{GameEvent, GameStartEvent}};
+use shared::{error::BunnyChessApiError, events::{GameEvent, GameOverEvent, GameStartEvent, GameStateUpdateEvent}};
 
 use crate::state::state::AppState;
 
@@ -13,6 +13,44 @@ pub async fn emit_game_start(state: &AppState, payload: GameStartEvent) -> Resul
         .publish(subject.to_string(), payload.into())
         .await
         .expect("Failed to emit game-start event");
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub async fn emit_game_state_update(
+    state: &AppState,
+    payload: GameStateUpdateEvent
+) -> Result<(), BunnyChessApiError> {
+    let event = GameEvent::GameStateUpdate(payload.clone());
+    let subject = event.subject();
+
+    let payload = serde_json::to_vec(&event).unwrap();
+
+    state
+        .jetstream
+        .publish(subject.to_string(), payload.into())
+        .await
+        .expect("Failed to emit game-state-update event");
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub async fn emit_game_over_event(
+    state: &AppState,
+    payload: GameOverEvent
+) -> Result<(), BunnyChessApiError> {
+    let event = GameEvent::GameOver(payload.clone());
+    let subject = event.subject();
+
+    let payload = serde_json::to_vec(&event).unwrap();
+
+    state
+        .jetstream
+        .publish(subject.to_string(), payload.into())
+        .await
+        .expect("Failed to emit game-over event");
 
     Ok(())
 }
