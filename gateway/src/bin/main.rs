74993cfe4d087a43411f50cb786fdc102237278e
Raymond Nambaale <nambaaler@gmail.com>
2025-11-26 19:23:35 +0300

    [gateway] Read env from manifest root

--- gateway/src/bin/main.rs
+++ gateway/src/bin/main.rs
@@ -8,19 +8,15 @@ use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Env
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let crate_dir = env!("CARGO_MANIFEST_DIR");
+    dotenvy::from_filename(format!("{}/.env", crate_dir)).ok();
+
     let app_env = match env::var("APP_ENV") {
         Ok(v) if v.trim() == "dev" => AppEnv::Dev,
         _ => AppEnv::Prod,
     };
 
     println!("Running in {app_env} mode");
-
-    if app_env == AppEnv::Dev {
-        match dotenvy::dotenv() {
-            Ok(path) => println!(".env read successfully from {}", path.display()),
-            Err(e) => panic!("Could not load .env file: {e}"),
-        };
-    }
 
     let ApiConfig {
         server,
